964568c0827bc333fcad80811bfc01be3205ef29
JordanBGonz <156833058+JordanBGonz@users.noreply.github.com>
2024-01-22 21:50:30 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework1
+{
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+       double z = 34.9;
+        Console.WriteLine(z = 4*x*x + 3*y);
+    }
+}
+}
